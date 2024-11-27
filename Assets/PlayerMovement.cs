14e95ef13c1e0581dbd3898d33d48845f16fe7f8
kch45 <kimch4577@gmail.com>
2024-11-27 22:23:47 +0900

    맵 외각 구현(외각으로 못 벗어남) 및 캐릭터 이동 구현해서 테스트 했습니다

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    Rigidbody2D rigid;
+    Vector3 movement;
+
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    //Physics engine Updates
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+        }
+        else if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            moveVelocity = Vector3.up;
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            moveVelocity = Vector3.down;
+        }
+
+        transform.position += moveVelocity * moveSpeed * Time.deltaTime;
+    }
+}
